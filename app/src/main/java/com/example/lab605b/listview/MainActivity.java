e72db61fa514913b7f6cc2f86d65cdf0fe7479f8
HRida <H.Rida1@hotmail.com>
2018-11-22 18:23:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lab605b/listview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.lab605b.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] carsArray = {"BMW","Mercedes","Honda","Toyota","Jaguar","Nissan","Buick","Mazda"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.listview, carsArray);
+
+        ListView listView = (ListView) findViewById(R.id.cars_list);
+
+        listView.setAdapter(adapter);
+    }
+}
